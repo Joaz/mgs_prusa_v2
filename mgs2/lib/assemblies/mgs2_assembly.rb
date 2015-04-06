7a59a8e799896c285f7ebffbf5fbb614a502a017
Jennfier Glauche <webmaster@joaz.de>
2015-04-06 21:46:36 +0200

    code cleanup, created basic top part

--- mgs2/lib/assemblies/mgs2_assembly.rb
+++ mgs2/lib/assemblies/mgs2_assembly.rb
@@ -4,16 +4,16 @@ class Mgs2Assembly < CrystalScad::Assembly
 		# Design goals
 		
 		# Mandetory:
-		# - Use of default MGS big gears and motor gears 	
-		# - Easier printing, easier assembly
+		# - Use of default MGS big gears and motor gears (done)
+		# - Easier printing, easier assembly 
 		# - Quick release idler
 		# - Support for J-head and Hexagons
 		# - Mount for both i3 standards, 24mm and 30mm holes
 
 		# Really nice to have:
-		# - Easy exchange of filament feed guides
-		# - Easy replacement of the gears
-		# - easy assembly
+		# - Easy exchange of filament feed guides (second proto)
+		# - Easy replacement of the gears (done)
+		# - easy assembly 
 		# - nice looking
 		# - Extrusion cooling fan
 		# - Adapter for i2s and ordbots
